b676ae7511002e736ad557e4d0985364d69b9f63
Nilkamal91 <60183091+Nilkamal91@users.noreply.github.com>
2020-02-06 19:48:32 +0530

    Create XXIBM_PRODUCT_STYLE.cs

---
+++ app/Models/XXIBM_PRODUCT_STYLE.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace app.Models  
+{  
+    public class XXIBM_PRODUCT_STYLE  
+    {  
+        private ProductStyle context;  
+  
+        public int Item_Number { get; set; }  
+  
+        public string Description { get; set; }  
+  
+        public string Long_Description { get; set; }  
+  
+        public int Catalogue_Category { get; set; }  
+  
+        public string Brand { get; set; }  
+    }  
+} 
