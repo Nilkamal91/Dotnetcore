878d51687753094af81a1864f235a0ce817ba7da
Nilkamal91 <60183091+Nilkamal91@users.noreply.github.com>
2020-02-06 19:48:23 +0530

    Create XXIBM_PRODUCT_STYLEController.cs

---
+++ app/Controllers/XXIBM_PRODUCT_STYLEController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using app.Models;
+
+namespace app.Controllers  
+{  
+    public class XXIBM_PRODUCT_STYLEController : Controller  
+    {  
+        public IActionResult Index()  
+        {  
+            ProductStyle context = HttpContext.RequestServices.GetService(typeof(app.Models.ProductStyle)) as ProductStyle;  
+  
+            return View(context.GetAllXXIBM_PRODUCT_STYLEs());  
+        }  
+  
+  
+    }  
+}
